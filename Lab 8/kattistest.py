cb6a1ea5a838d0482a994ea6f4006375cf14c665
Karolin Valaszkai <karolinv@kth.se>
2019-11-01 14:12:48 +0100

    Updated assertequal (a, c, 8)

--- Lab 8/kattistest.py
+++ Lab 8/kattistest.py
@@ -4,7 +4,10 @@ from Lab8 import *
 
 #T ex kan ett test vara att kön innehåller en syntaktiskt korrekt molekyl,
 # som A -> a -> 5
-# (OBS! De fem funktionerna ska skrivas i nästa punkt.)
+
+
+
+
 class SyntaxTest(unittest.TestCase):
 
     """CORRECT/WRONG"""
@@ -20,9 +23,9 @@ class SyntaxTest(unittest.TestCase):
 
 
     def testWrongUPC(self):
-        self.assertEqual(check_syntax("a"), "Fel, borde vara uppercase: ")
-        self.assertEqual(check_syntax("cr12"), "Fel, borde vara uppercase: ")
-        self.assertEqual(check_syntax("8"), "Fel, borde vara uppercase: ")
+        self.assertEqual(check_syntax("a"), "Fel, borde vara uppercase: a")
+        self.assertEqual(check_syntax("cr12"), "Fel, borde vara uppercase: c")
+        self.assertEqual(check_syntax("8"), "Fel, borde vara uppercase: 8")
 
 
     def testWrongNum(self):
