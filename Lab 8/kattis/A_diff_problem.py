0145b377e1d672b51576365a6c6445c18d734ad7
Karolin Valaszkai <karolinv@kth.se>
2019-11-01 14:11:52 +0100

    Added comments

--- Lab 8/kattis/A_diff_problem.py
+++ Lab 8/kattis/A_diff_problem.py
@@ -1,6 +1,9 @@
 import sys
 
-for row in sys.stdin:
+
+"Compute the difference between non-negative integers."
+
+for row in sys.stdin:   #standard input
     xy = row.split()
     x = int(xy[0])
     y = int(xy[1])
