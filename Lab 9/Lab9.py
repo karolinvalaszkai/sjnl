1cb509cc15fed532e0a73aa06d2962a12de97773
Karolin Valaszkai <karolinv@kth.se>
2019-11-06 17:11:20 +0100

    U_test2: 6 tests passed

--- Lab 9/Lab9.py
+++ Lab 9/Lab9.py
@@ -13,6 +13,15 @@ class Syntaxfel(Exception):
     pass
 
 
+atomlist = ["H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K",
+            "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb",
+            "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe", "Cs",
+            "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta",
+            "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th", "Pa",
+            "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt",
+            "Ds", "Rg", "Cn", "Fl", "Lv"]
+
+
 #[21] ej [2,1]
 
 #FRÅGA!
@@ -84,6 +93,8 @@ def read_group(q):
             if next.isdigit():
                 num(next, q)
                 return
+            else:
+                raise Syntaxfel("Saknad siffra vid radslutet " + printQueue(q))
         else:
             raise Syntaxfel("Saknad högerparentes vid radslutet ")
 
@@ -93,33 +104,49 @@ def read_group(q):
 
 
 def atom(first_char, q, next):
-    upper_case_l(first_char)  #H
+    upper_case_l(first_char, next)  #H
+    atom_name = first_char
+    if next != None and next.islower():
+        atom_name += next  # vad händer om next == None
 
-    if next == None:
-        return
+    if atom_name in atomlist:
 
-    if next.isdigit() or next.isupper() or next == "(" or next == ")": # Om next == digit eller Stor bokstav
-        return # ska vi kolla  num, "(" eller ")" i denna funktion
+        upper_case_l(first_char, next)  # H
 
-    elif next is not None:
-        lower_case_l(next)
+        if next == None:
+            return
+
+        if next.isdigit() or next.isupper() or next == "(" or next == ")":  # Om next == digit eller Stor bokstav
+            return  # ska vi kolla  num, "(" eller ")" i denna funktion
+
+        elif next is not None:
+            lower_case_l(next)
+            q.dequeue()
+            return
+
+    if q.peek().islower():
         q.dequeue()
-        return
-
-    raise Syntaxfel("Okänd atom vid radslutet") + next #+vadå??)
+    raise Syntaxfel("Okänd atom vid radslutet " + printQueue(q))
 
 
 """UPPER CASE LETTER"""
 #< LETTER >::= A | B | C | ... | Z
 
 
-def upper_case_l(first_char):
+def upper_case_l(first_char, next):
     #check both for int or letter
     if first_char.isupper(): #P if sant
         return
 
-    raise Syntaxfel("Saknad stor bokstav vid radslutet " + first_char) #+ first_char) #eller hela atomnamnet?
+    else:
 
+        if next is not None:
+
+            raise Syntaxfel("Saknad stor bokstav vid radslutet " + first_char + next)
+
+        else:
+
+            raise Syntaxfel("Saknad stor bokstav vid radslutet " + first_char)
 
 """LOWER CASE LETTER"""
 #< letter >::= a | b | c | ... | z
@@ -138,30 +165,38 @@ def lower_case_l(next):
 
 def num(next, q):
 
-    sum_int = ''
 
-    # while, körs tills påståendet är sant
-    while next is not None and next.isdigit():  # kolla ifall number, jsdigit() pga string
+    if next is not '0':
 
-        sum_int += next
+        sum_int = ''
 
-        q.dequeue()
+        # while, körs tills påståendet är sant
+        while next is not None and next.isdigit():  # kolla ifall number, jsdigit() pga string
 
-        next = q.peek()
+            sum_int += next
+
+            q.dequeue()
+
+            next = q.peek()
 
 
-    if int(sum_int) > 1:
-        return q
+        if int(sum_int) > 1:
+            return q
 
-    raise Syntaxfel("För litet tal vid radslutet " + next)
+    else:
+        q.dequeue()  # pga 0
+
+    raise Syntaxfel("För litet tal vid radslutet " + printQueue(q))
 
 
 def printQueue(q):
+    return_string = ""
     while not q.isEmpty():
         word = q.dequeue()
-        print(word, end = " ")
+        print(word, end=" ")
         print()
-
+        return_string += word
+    return return_string
 
 def storeSentence(atom_name):
     q = LinkedQ()
